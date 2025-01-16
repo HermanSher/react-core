33bfe4559f053cf6b8f92a01731811b80513fc3d
HermanSher <hermansher1111@gmail.com>
2025-01-17 00:30:19 +0530

    React Core Learning The Core

---
+++ App.js
@@ -0,0 +1,31 @@
+// const heading = React.createElement("h1", {}, "Hello World From React");
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// Nested React Elements
+// const div = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "H1 nested from reaact")
+//   )
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(div);
+
+const div = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "H1 nested from reaact"),
+    React.createElement("h2", {}, "H2 nested from react"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h3", {}, "H3 nested from reaact"),
+    React.createElement("h4", {}, "H4 nested from react"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(div);
